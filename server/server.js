5b2b95189208c13c978421e9a1b308b3fef622d6
James Jacobsen <edjcjacobsen@gmail.com>
2024-01-20 11:30:45 -0600

    got a basic server spun up

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+// require("dotenv").config();
+
+// const chatRouter = require("./routes/chat.router.js");
+// app.use("/api/chat", chatRouter);
+
+// Serve static files
+app.use(express.static("build"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
